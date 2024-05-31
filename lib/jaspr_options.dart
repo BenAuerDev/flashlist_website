dcf6f181996f18099fd3cce076a84394b8bf1ac5
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-05-31 13:15:15 +0200

    add gitignore

--- lib/jaspr_options.dart
+++ lib/jaspr_options.dart
@@ -3,8 +3,8 @@
 
 import 'package:jaspr/jaspr.dart';
 
-import 'pages/about.dart' as c0;
-import 'pages/home.dart' as c1;
+import 'pages/home.dart' as c0;
+import 'pages/about.dart' as c1;
 
 /// Default [JasprOptions] for use with your jaspr project.
 ///
@@ -24,7 +24,7 @@ import 'pages/home.dart' as c1;
 /// ```
 const defaultJasprOptions = JasprOptions(
   targets: {
-    c0.About: ClientTarget<c0.About>('pages/about'),
-    c1.Home: ClientTarget<c1.Home>('pages/home'),
+    c0.Home: ClientTarget<c0.Home>('pages/home'),
+    c1.About: ClientTarget<c1.About>('pages/about'),
   },
 );
