3f428472213c10d3d4410bb752bcb9b7b5011618
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-06-05 13:06:57 +0200

    misc changes

--- lib/jaspr_options.dart
+++ lib/jaspr_options.dart
@@ -6,6 +6,7 @@ import 'package:jaspr/jaspr.dart';
 import 'pages/home.dart' as c0;
 import 'pages/about.dart' as c1;
 import 'components/home/feature_gallery.dart' as c2;
+import 'components/home/cta_bar.dart' as c3;
 
 /// Default [JasprOptions] for use with your jaspr project.
 ///
@@ -27,6 +28,7 @@ const defaultJasprOptions = JasprOptions(
   targets: {
     c0.Home: ClientTarget<c0.Home>('pages/home'),
     c1.About: ClientTarget<c1.About>('pages/about'),
-    c2.Home: ClientTarget<c2.Home>('components/home/feature_gallery'),
+    c2.FeatureGallery: ClientTarget<c2.FeatureGallery>('components/home/feature_gallery'),
+    c3.CallToActionBar: ClientTarget<c3.CallToActionBar>('components/home/cta_bar'),
   },
 );
