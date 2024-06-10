8e747e58c38ff42ee182d16be207f491acc5af2c
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-06-10 12:03:32 +0200

    update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:flashlist_website/components/home/hero_banner.dart';
-import 'package:flashlist_website/components/home/cta_bar.dart';
+import 'package:flashlist_website/components/cta_bar.dart';
 import 'package:flashlist_website/components/home/feature_gallery.dart';
 import 'package:jaspr/jaspr.dart';
 
@@ -13,21 +13,31 @@ class Home extends StatelessComponent {
       HeroBanner(),
       CallToActionBar(),
       div(classes: 'content-wrapper', [
-        h1([text('FlashList')]),
+        h1([text('Flashlist: Simplify Your Task World')]),
         h2([
-          text('Because communicating your ideas should be easy.'),
+          text('Effortlessly collect, organize and share your thoughts.'),
         ]),
         p([
           text(
-            'Flashlist is a cutting-edge list management tool. Designed to be simple and intuitive, it allows you to create a list and fill it with your ideas quickly. Also Collaboration is easy like never before thanks to the ability to share lists and work together in real-time.',
-          ),
+              'Imagine having all your ideas, tasks, and notes in one place. With FlashList, you can effortlessly collect, organize, and share your thoughts. Whether you\'re planning a project, managing a team, or just trying to stay on top of your to-do list, we\'ve got you covered.'),
         ]),
         FeatureGallery(),
+        p(
+          [
+            text(
+              'By using FlashList, you can stay organized, focused, and in control. Try it out and see how it can simplify your task world!',
+            ),
+          ],
+          styles: Styles.box(
+            margin: EdgeInsets.only(top: 44.px),
+          ),
+        ),
       ]),
     ]);
   }
 
   static get styles => [
         css('p').text(align: TextAlign.center),
+        css('h2').box(margin: EdgeInsets.only(top: 8.px, bottom: 24.px))
       ];
 }
