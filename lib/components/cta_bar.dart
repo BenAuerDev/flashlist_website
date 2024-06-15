1ba8902ad01245859c2f96ff868d60183976d6d6
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-06-15 21:26:00 +0200

    link to google play store

--- lib/components/cta_bar.dart
+++ lib/components/cta_bar.dart
@@ -20,7 +20,8 @@ class CallToActionBar extends StatelessComponent {
         ),
         a(
           classes: 'cta-button',
-          href: 'https://play.google.com/store/flashlist',
+          href:
+              'https://play.google.com/store/apps/details?id=com.flashlistapp',
           target: Target.blank,
           [
             img(classes: 'cta-button-icon', src: 'images/playstore.svg'),
